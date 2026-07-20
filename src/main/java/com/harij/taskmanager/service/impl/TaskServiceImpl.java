99b28abe42dfb9d35bc4ac274370b5f56a9fe327
shiva2490 <thumatishivashankar.com>
2026-07-20 20:47:48 +0530

    Implement TaskService business logic

---
+++ src/main/java/com/harij/taskmanager/service/impl/TaskServiceImpl.java
@@ -0,0 +1,98 @@
+package com.harij.taskmanager.service.impl;
+
+import com.harij.taskmanager.dto.CreateTaskRequest;
+import com.harij.taskmanager.model.Task;
+import com.harij.taskmanager.model.TaskStatus;
+import com.harij.taskmanager.service.TaskService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TaskServiceImpl implements TaskService {
+
+    // Stores all tasks in memory
+    private final List<Task> tasks = new ArrayList<>();
+
+    // Generates unique IDs
+    private Long nextId = 1L;
+
+    /**
+     * Loads sample tasks when the application starts.
+     */
+    @PostConstruct
+    public void loadSampleTasks() {
+
+        tasks.add(new Task(
+                nextId++,
+                "Learn Spring Boot",
+                "Complete REST API assignment",
+                TaskStatus.PENDING,
+                LocalDateTime.now()
+        ));
+
+        tasks.add(new Task(
+                nextId++,
+                "Practice Angular",
+                "Prepare UI screens",
+                TaskStatus.COMPLETED,
+                LocalDateTime.now()
+        ));
+    }
+
+    @Override
+    public Task createTask(CreateTaskRequest request) {
+
+        // Create a new task object
+        Task task = new Task(
+                nextId++,
+                request.getTaskName(),
+                request.getDescription(),
+                TaskStatus.PENDING,
+                LocalDateTime.now()
+        );
+
+        // Store task in memory
+        tasks.add(task);
+
+        // Return created task
+        return task;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+
+        // Return a copy of all tasks
+        return new ArrayList<>(tasks);
+    }
+
+    @Override
+    public Task updateTaskStatus(Long id) {
+        Task task = tasks.stream()
+                .filter(t -> t.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+
+        // Update the task status
+        task.setStatus(TaskStatus.COMPLETED);
+
+        //Return updated task
+        return task;
+    }
+
+    @Override
+    public void deleteTask(Long id) {
+
+        // Remove task if ID matches
+        boolean removed = tasks.removeIf(t -> t.getId().equals(id));
+
+        // Throw exception if task not found
+        if (!removed) {
+            throw new RuntimeException("Task not found");
+        }
+    }
+}
